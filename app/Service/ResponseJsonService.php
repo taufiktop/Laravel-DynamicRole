288c8ffaaf40e0bd72ef7de29a00ad2787f6e792
Taufik Sarbiyanto <taufiksarbiyanto@gmail.com>
2024-02-07 23:00:06 +0700

    update service json response

--- app/Service/ResponseJsonService.php
+++ app/Service/ResponseJsonService.php
@@ -4,12 +4,12 @@ namespace App\Service;
 
 class ResponseJsonService
 {
-    public function success($data = [], $message = 'Success', )
+    public function success($data = [], $message = 'Success')
     {
         if(count($data)>0) {
             return response()->json(array(
                 'OUT_STAT' => 'T',
-                'OUT_MESS' => 'Success',
+                'OUT_MESS' => $message,
                 'OUT_DATA' => $data,
                 'TOTAL_DATA' => count($data)
             ), 200);
@@ -17,7 +17,7 @@ class ResponseJsonService
         else {
             return response()->json(array(
                 'OUT_STAT' => 'T',
-                'OUT_MESS' => 'Success',
+                'OUT_MESS' => $message,
                 'OUT_DATA' => [],
                 'TOTAL_DATA' => count($data)
             ), 200);
